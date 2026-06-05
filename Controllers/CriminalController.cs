e02fe770014a1af98528c292f838204bc9cda6ad
Khadiaj-tul-kubra <Kubrakt619@gmail.com>
2026-06-05 01:33:40 -0700

    Implemented FIR Management Module

---
+++ Controllers/CriminalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CriminalManagementSystem.Models;
+using CriminalManagementSystem.Services;
+using MongoDB.Driver;
+
+namespace CriminalManagementSystem.Controllers
+{
+    public class CriminalController : Controller
+    {
+        private readonly MongoDBService _mongoDBService;
+
+        public CriminalController(MongoDBService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        public IActionResult Index()
+        {
+            var criminals = _mongoDBService.Criminals
+                .Find(_ => true)
+                .ToList();
+
+            return View(criminals);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Criminal criminal)
+        {
+            _mongoDBService.Criminals.InsertOne(criminal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
